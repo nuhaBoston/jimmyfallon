2dfe2d0dd52b53954d82664c4605887bbbc2af60
nuhaBoston <39391126+nuhaBoston@users.noreply.github.com>
2019-04-17 17:19:33 -0400

    jimmy

--- jimmy_fallon.c
+++ jimmy_fallon.c
@@ -10,12 +10,7 @@ sem_t connected_lock;
 int next_id;
 
 void* phonecall(void* vargs){
-
-/* static sem_t connected_lock;
-    sem_wait(&operators);
-    sem_post(&operators);
-    printf("We are trying to connect you!");*/
-    static int id;
+    int id;
     id = next_id;
     next_id++;
     static int NUM_OPERATORS = 3;
@@ -23,54 +18,59 @@ void* phonecall(void* vargs){
     static int connected = 0; 
     //tells how many callers are currently connected
     while(1){
-        printf("Thread %d attempting to call!", id);
+        printf("Thread %d attempting to call!\n", id);
         //opening the connected sem that will be checked in the critical section
         sem_wait(&connected_lock);
-        if(connected == NUM_LINES){
-            //lock it
-            sem_post(&connected_lock);
-            printf("The lines are busy right now!"); 
-            sleep(8);  
-        }
-        else{ 
+        
+        if(connected != NUM_LINES){ 
             connected++;
             sem_post(&connected_lock);
             printf("Thread %d has available line,call ringing\n",id);
-            sleep(8);
             sem_wait(&operators);
             printf("Thread %d is talking to the operator\n",id);
-            sleep(5);
-            printf("Thread %d has got a ticket", id);
+            sleep(2);
+            printf("Thread %d has got a ticket\n", id);
             sem_post(&operators);
             sem_wait(&connected_lock);
             connected--;
             sem_post(&connected_lock);
-            printf("Thread %d has hung up", id);
+            printf("Thread %d has hung up\n", id);
             break;
+        }
+        else{
+            //lock it
+            sem_post(&connected_lock);
+            printf("The lines are busy right now!\n"); 
+            sleep(2);  
         }
 
     }
    
    }
 
-int main(){
-    int numCallers = 0;
+int main(int argc, char* argv[]){
+     sem_init(&connected_lock, 0, 1);
+    //initialize the 3 operators
+    sem_init(&operators,0,3);
+    sem_init(&callers, 0, 5);
+    int numCallers;
     next_id = 1;
-    printf("How many callers do you want?\n");
-    scanf("%d", &numCallers);
+    if(argc == 1){
+        numCallers = 10;
+    }
+    else{
+        numCallers = atoi(argv[1]);
+    }
+    
+   
     pthread_t tid[numCallers];
     for(int i = 0; i < numCallers; i++){
         int num1 = pthread_create(&tid[i],NULL,phonecall,NULL);
-        sleep(8);
         if(num1 != 0){
             EXIT_FAILURE;
         }
     }
-    sem_init(&connected_lock, 0, 1);
-    //initialize the 3 operators
-    sem_init(&operators,0,3);
-    sem_init(&callers, 0, 5);
-
+   
    for(int j = 0; j < numCallers; j++){
       int num2 = pthread_join(tid[j],NULL);
       if(num2 != 0){
